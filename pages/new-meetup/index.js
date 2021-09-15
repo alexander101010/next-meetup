27ad6a31ab6d80e65724dd417829af370433ced6
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-15 23:31:05 +0200

    new meetup form page with api post

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,26 @@
 // domain/new-meetup
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  const addMeetupHandler = (enteredMeetupData) => {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
     console.log(enteredMeetupData);
-  };
+    // send request - use local path since its an interior api served on same server
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push('/');
+  }
+
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
