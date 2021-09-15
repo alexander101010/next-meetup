60e3305384c33040d29da1bc087d4a20e0c12c4d
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-15 10:51:44 +0200

    move layout page wrapper to MyApp in app partial

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,16 +1,11 @@
 // domain/new-meetup
-import Layout from '../../components/layout/Layout';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
   const addMeetupHandler = (enteredMeetupData) => {
     console.log(enteredMeetupData);
   };
-  return (
-    <Layout>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
-    </Layout>
-  );
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeetupPage;
