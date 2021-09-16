22a8ae5ce28de0ce08450753a05a3251bf3a2a69
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-16 12:00:14 +0200

    delete dummy data-get DB data w/ getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,10 @@
-// import { useEffect, useState } from 'react';
-import MeetupList from '../components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'Black Tie Gala',
-    image:
-      'https://images.unsplash.com/photo-1595792463990-07008351a4fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-    address: 'Antartica',
-    description: `Dress to impress. Bring out the penguin suits!`,
-  },
-  {
-    id: 'm2',
-    title: 'Goring of the Humans',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/4/48/Sanfermines_Vaquillas_Pamplona_03.jpg',
-    address: 'Pamploma',
-    description: `Usually I don't victim blame, but they asked for it.`,
-  },
-  {
-    id: 'm3',
-    title: 'Summering in Cali',
-    image:
-      'https://static01.nyt.com/images/2019/10/01/science/00TB-WHALESONGS1/00TB-WHALESONGS1-superJumbo.jpg',
-    address: 'California',
-    description: `Where my humpbacks at? Pod will be cruising by Santa Monica this weekend, join us! (And don't forget next weekend is the Catalina Wine Mixer!)`,
-  },
-];
+import MeetupList from '../components/meetups/MeetupList';
+import { MONGO_STRING } from '../js/config';
 
 function HomePage(props) {
+  // this useEffect approach only runs once as page is rendered, wouldn't catch updates to db, use getStaticProps export instead
   // const [loadedMeetups, setLoadedMeetups] = useState([]);
   // useEffect(() => {
   //   // send http request to fetch meetups data - mimick with dummy data
@@ -37,10 +13,9 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-// export async function getServerSideProps(context) {
+// export async function getServerSideProps(context) { // better for data that changes many times per second
 //   // runs on the server after deployment, not during the build process
 //   // can use credentials that shouldn't be exposed to users
-//   // fetch data from an API
 //   const req = context.req;
 //   const res = context.res;
 //   return {
@@ -50,10 +25,25 @@ function HomePage(props) {
 //   };
 // }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  // not exposed to the client
+  // fetch data
+  const client = await MongoClient.connect(MONGO_STRING);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
