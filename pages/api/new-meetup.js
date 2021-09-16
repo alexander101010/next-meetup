a3c1f14098e98b2ca8b5e4b1bcf5027480d45178
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-16 12:01:03 +0200

    refactor api db string

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,15 +1,14 @@
 // route: /api/new-meetup
 // POST /api/new-meetup
 import { MongoClient } from 'mongodb';
+import { MONGO_STRING } from '../../js/config';
 
 async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
     // const { title, image, address, description } = data;
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://alex:QpYzzoIMmqOLfk70@cluster0.i3k3f.mongodb.net/meetup?retryWrites=true&w=majority'
-    );
+    const client = await MongoClient.connect(MONGO_STRING);
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups'); // can be same as db name but doesn't have to be
