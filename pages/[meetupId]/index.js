a6eba34e28a1708d377a0beb5a756017de8add4c
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-17 17:08:32 +0200

    connect getStaticPaths/Props to db, add metadata

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,37 +1,44 @@
 // dynamic show page, folder name can have bracketed identifier
-import MeetupDetails from '../../components/meetups/MeetupDetails';
+import Head from 'next/head';
+import { MongoClient, ObjectId } from 'mongodb';
+import { MONGO_STRING } from '../../js/config';
 
-function MeetupDetailsPage() {
+import MeetupDetails from '../../components/meetups/MeetupDetails';
+import { Fragment } from 'react';
+
+function MeetupDetailsPage(props) {
   return (
-    <MeetupDetails
-      image='https://images.unsplash.com/photo-1595792463990-07008351a4fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-      title='Black Tie Gala'
-      description='Dress to impress. Bring out the penguin suits!'
-      address='Antartica'
-    />
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name='description' content={props.meetupData.description} />
+      </Head>
+      <MeetupDetails
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        description={props.meetupData.description}
+        address={props.meetupData.address}
+      />
+    </Fragment>
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(MONGO_STRING);
+  // console.log(client);
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  // pass empty object to filter arg to get all objects, then just get the id field by passing {_id:1} as second arg
+  client.close();
   return {
     fallback: false, // means all paths are below, if true you can just pregenerate some and the rest will be done dynamically.
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm3',
-        },
-      },
-    ],
+    })),
   };
 }
 
@@ -39,17 +46,24 @@ export async function getStaticProps(context) {
   // you can use CONTEXT arg to get PARAMS - the ID from the url
   const meetupId = context.params.meetupId;
   // fetch data for single meetup using meetupId from Params
-  console.log(meetupId);
+  const client = await MongoClient.connect(MONGO_STRING);
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
 
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          'https://images.unsplash.com/photo-1595792463990-07008351a4fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-        title: 'Black Tie Gala',
-        description: 'Dress to impress. Bring out the penguin suits!',
-        address: 'Antartica',
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
